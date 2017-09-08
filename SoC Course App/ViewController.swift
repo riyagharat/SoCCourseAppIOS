d35d8e95db31adb2d33ea01cc6198f9d4383b851
riya <riya@Sumeets-MacBook-Pro.local>
2017-09-08 12:07:58 -0400

    Initial Commit

---
+++ SoC Course App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoC Course App
+//
+//  Created by riya on 9/8/17.
+//  Copyright © 2017 riya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
